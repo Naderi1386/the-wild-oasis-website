09cbb41144b4a3973b79a57886b810ec13acbcbc
unknown <mh.naderi1386@gmail.com>
2025-03-08 16:49:23 +0330

    add a client component

--- app/_components/SideNavigation.tsx
+++ app/_components/SideNavigation.tsx
@@ -1,3 +1,4 @@
+"use client"
 import {
   CalendarDaysIcon,
   HomeIcon,
@@ -5,6 +6,7 @@ import {
 } from '@heroicons/react/24/solid';
 import SignOutButton from './SignOutButton';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface navLinkType{
   name:string
@@ -31,13 +33,18 @@ const navLinks:navLinkType[] = [
 ];
 
 function SideNavigation() {
+  const pathname=usePathname()
   return (
-    <nav className='border-r border-primary-900 h-full'>
-      <ul className='flex flex-col gap-2 h-full text-lg'>
+    <nav className="border-r border-primary-900 h-full">
+      <ul className="flex flex-col gap-2 h-full text-lg">
         {navLinks.map((link) => (
           <li key={link.name}>
             <Link
-              className={`py-3 px-5 hover:bg-primary-900 hover:text-primary-100 transition-colors flex items-center gap-4 font-semibold text-primary-200`}
+              className={`py-3 px-5 ${
+                link.href === pathname
+                  ? " bg-primary-900 text-primary-100"
+                  : "hover:bg-primary-900 hover:text-primary-100"
+              }   transition-colors flex items-center gap-4 font-semibold text-primary-200`}
               href={link.href}
             >
               {link.icon}
@@ -46,7 +53,7 @@ function SideNavigation() {
           </li>
         ))}
 
-        <li className='mt-auto'>
+        <li className="mt-auto">
           <SignOutButton />
         </li>
       </ul>
