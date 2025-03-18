24e75976d37e99f39d88d790f1542bee93184ca1
unknown <mh.naderi1386@gmail.com>
2025-03-18 20:21:27 +0330

    changing ui

--- app/_lib/actions/auth.ts
+++ app/_lib/actions/auth.ts
@@ -3,8 +3,8 @@
 import { signIn, signOut } from "@/auth";
 
 export const login = async () => {
-  await signIn("github", { redirectTo: "/" });
+  await signIn("github", { redirectTo: "/account" });
 };
 export const logout = async () => {
-  await signOut({ redirectTo: "/" });
+  await signOut({ redirectTo: "/account" });
 };
