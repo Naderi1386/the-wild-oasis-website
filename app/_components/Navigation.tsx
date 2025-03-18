9c547c011e4832e9a1e068779cf7b7b77f72a16e
unknown <mh.naderi1386@gmail.com>
2025-03-18 17:05:21 +0330

    changing ui

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,6 +1,8 @@
+
 import Link from "next/link";
 
-export default function Navigation() {
+export default  function Navigation() {
+  
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
