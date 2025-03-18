7bfb77e7c5cf3067b4201b472c2c4f673ea930a8
unknown <mh.naderi1386@gmail.com>
2025-03-18 18:01:48 +0330

    add a avatar

--- app/_components/Navigation.tsx
+++ app/_components/Navigation.tsx
@@ -1,28 +1,46 @@
 
+import { auth } from "@/auth";
 import Link from "next/link";
 
-export default  function Navigation() {
+export default async  function Navigation() {
+  const session=await auth()
   
   return (
     <nav className="z-10 text-xl">
       <ul className="flex gap-16 items-center">
         <li>
-          <Link href="/cabins" className="hover:text-accent-400 transition-colors">
+          <Link
+            href="/cabins"
+            className="hover:text-accent-400 transition-colors"
+          >
             Cabins
           </Link>
         </li>
         <li>
-          <Link href="/about" className="hover:text-accent-400 transition-colors">
+          <Link
+            href="/about"
+            className="hover:text-accent-400 transition-colors"
+          >
             About
           </Link>
         </li>
         <li>
-          <Link
-            href="/account"
-            className="hover:text-accent-400 transition-colors"
-          >
-            Guest area
-          </Link>
+          {session?.user?.image ? (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors flex items-center gap-4"
+            >
+              <img referrerPolicy="no-referrer" className="h-8 rounded-full" src={session.user.image} alt={`Avatar-${session.user.name}`} />
+              <span> Guest area</span>
+            </Link>
+          ) : (
+            <Link
+              href="/account"
+              className="hover:text-accent-400 transition-colors"
+            >
+              Guest area
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
